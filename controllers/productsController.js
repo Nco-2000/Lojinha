f3f4d07d6867b6fcd6ce29ac03fc613d69ab0d64
Nco-2000 <n.chiaradia2000@gmail.com>
2024-11-18 10:37:50 -0300

    productsController vazio

---
+++ controllers/productsController.js
@@ -0,0 +1,30 @@
+
+const {Router} = require('express');
+
+const router = Router();
+
+//Lista dos produtos.
+router.get('/products', async(req, res) => {
+    
+
+})
+
+//Ver Produto individual.
+router.get('/products/:id', async(req, res) => {
+
+})
+
+//Adicionar Produto.
+router.post('/products/:id', async(req, res) => {
+
+})
+
+//Editar Produto.
+router.patch('/products/:id', async(req, res) => {
+
+})
+
+//Deletar Produto.
+router.delete('/products/:id', async(req, res) => {
+
+})
